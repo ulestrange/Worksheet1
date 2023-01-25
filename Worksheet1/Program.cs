d00fdd9a3894c5a54a69a0805b21d1e667a6aa1d
Una L <una.lestrange@atu.ie>
2023-01-25 09:48:45 +0000

    Add project files.

---
+++ Worksheet1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Worksheet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
